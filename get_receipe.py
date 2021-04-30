7047bdc1394d64167daaa5846b21984807f12e80
AXXIAR <galoualexis@gmail.com>
2021-04-30 13:56:35 +0200

    commande receipe

--- get_receipe.py
+++ get_receipe.py
@@ -1,47 +1,55 @@
-import feedparser
-from bs4 import BeautifulSoup
+import feedparser               # module récupérant le contenu de flux RSS
+from bs4 import BeautifulSoup   # module qui lit des pages html
+import json
 
 
+recettes_flux = feedparser.parse("https://cuisine-libre.org/?page=backend") # on récupère le contenu du flux dans 'recettes'
+
+recette1 = recettes_flux.entries[0]   # on récupère la première recette
 
 
-recettes = feedparser.parse("https://cuisine-libre.org/?page=backend")
+html_to_str1 = BeautifulSoup(recette1.content[0].value,features='html.parser')               #ici on enlève tous les éléments d'html pour garder que le texte
 
-yo = recettes.entries[0].content[0].value
-print(yo)
-soup1 = BeautifulSoup(yo)
+#ingrédients
+ing = []
+for ingredient in html_to_str1.findAll('div',{'class':'rss_texte'}):
+    ing.append(ingredient)
+res = BeautifulSoup(str(ing[0]),features='html.parser')
 
-images = []
-
-for img in soup1.findAll('img'):
-    images.append(img.get('src'))
-
-print(f"Image recette : {images[0]}")
-
-a= []
-for ingredient in soup1.findAll('div',{'class':'rss_texte'}):
-    a.append(ingredient)
-print(a[0])
-
-res = BeautifulSoup(str(a[0]))
-
-print(res.get_text())
-
-soup = BeautifulSoup(recettes.entries[0].summary)
+#tags
+tags_list = []
+for tag in recette1.tags:
+    tags_list.append(tag.term)
 
 
+#prépa
+prepa = ''
+html_to_str2 = BeautifulSoup(recette1.summary,features='html.parser')
 
-print(f"Recette : {recettes.entries[0].title}")
-print(f"Lien : {recettes.entries[0].link}")
-print(f"Auteur : {recettes.entries[0].author}")
-
-
-for tag in recettes.entries[0].tags:
-    print(tag.term)
-
-recette = ''
-for lettre in soup.get_text():
+for lettre in html_to_str2.get_text():
     if lettre == "-" :
         break
-    recette += lettre
+    prepa += lettre
 
-print(recette)
+#image lien
+
+images = []
+for img in html_to_str1.findAll('img'):
+    images.append(img.get('src'))
+
+
+
+recette1 = {
+    'name': str(recette1.title),
+    'link': str(recette1.link),
+    'author' : str(recette1.author),
+    'img_link' : str(images[0]),
+    'tags' : str(tags_list),
+    'tutorial' : {
+        "ingredients" : str(res.get_text()),
+        "preparation" : str(prepa)
+    }
+}
+
+with open('receipe.json', 'w') as json_file:
+    json.dump(recette1, json_file)
